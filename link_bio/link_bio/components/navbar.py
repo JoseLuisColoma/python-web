0aea784bdcaed5350941a4d9a64bd4674b6c39db
Jose Luis Coloma <colomatormojl@gmail.com>
2024-04-07 23:25:12 +0200

    commit 1

---
+++ link_bio/link_bio/components/navbar.py
@@ -0,0 +1,31 @@
+import reflex as rx
+
+
+def navbar() -> rx.Component:
+    return rx.hstack(
+        rx.hstack(
+            rx.image(src="/favicon.ico", width="2em"),
+            rx.heading("joco-dev", font_size="2em"),
+        ),
+        rx.spacer(),
+        rx.menu.root(
+            rx.menu.trigger(
+                rx.button("Menu"),
+            ),
+            rx.menu.content(
+                rx.menu.item("item 1"),
+                rx.menu.separator(),
+                rx.menu.item("Item 2"),
+                rx.menu.separator(),
+                rx.menu.item("Item 3"),
+                width="10rem",
+            ),
+        ),
+        position="fixed",
+        top="0px",
+        background_color="lightgray",
+        padding="1em",
+        height="4em",
+        width="100%",
+        z_index="5",
+    )
---
+++ link_bio/link_bio/link_bio.py
@@ -0,0 +1,14 @@
+
+import reflex as rx
+from link_bio.components.navbar import navbar
+
+
+class State(rx.State):
+    pass
+
+def index() -> rx.Component:
+    return navbar()
+
+
+app = rx.App()
+app.add_page(index)
